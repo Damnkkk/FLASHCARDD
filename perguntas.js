6ff3058222052404f5d537c2e5a30f85dc299012
Damn_5k <142242060+Damnkkk@users.noreply.github.com>
2024-11-12 10:44:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jogos',
+    'Qual o jogo mais vendido?',
+    'Minecraft'
+)
+
+criaCartao(
+    'Jogos',
+    'Quais os jogos mais jogados do Brasil?',
+    'Roblox e Fornite'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual o melhor jogo da historia?',
+    'Red Dead Redenmption'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
